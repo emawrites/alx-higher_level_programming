ef820bd9d0a86da2a69641866486b8e599cb0f4e
Emawrites <talkema2@gmail.com>
2022-07-20 23:50:32 +0000

    task 0 update

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 
-"""Writing an empty class that defines a Square."""
+"""Define a class Square."""
+
+
 class Square:
-	"""empty class."""
-pass
+    """Represent a square."""
+    pass
